a7e70a59b064afc930c69b449f0a3c8113dd2287
panchagirikavya12 <panchagirikavya12@gmail.com>
2026-07-18 21:55:55 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="kavya@123",
+    password="Your_MySql_Password",
     database="lost_found"
 )
 
